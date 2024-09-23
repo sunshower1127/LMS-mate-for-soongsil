6f53398c171714a18e2cb02abb0294662e2fb5f5
Sunwoo Kim (Mac) <sunshower1127@gmail.com>
2024-09-23 18:52:45 +0900

    수정: Sw 최신화와 함께 옵션 사용법 변경 수정: 아이디 치고 탭 눌러서 비밀번호 입력

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+숭실대학교 LMS 동영상 강의를 자동으로 시청하는 프로그램입니다.
+"""
+
 from sw_selenium.driver import SwChrome
 from sw_selenium.ui_manager import get_data_from_file_or_ui
 
@@ -9,23 +13,20 @@ id, pw = get_data_from_file_or_ui(
 
 ban_list = get_data_from_file_or_ui(
     file_path="금지할강의명.txt",
-    prompt_message="금지할 강의명을 입력해주세요.\n(여러개입력가능, 더이상 입력하지 않으려면 취소)",
+    prompt_message="금지할 강의명을 입력해주세요.\n(여러개 입력가능, 더이상 입력하지 않으려면 취소)",
     length="INF",
 )
 
-web = SwChrome(
-    timeout=10, keep_browser_open=False, prevent_sleep=True, popup=False, headless=True
-)
+web = SwChrome("disable_popup", "headless", "mute_audio", "prevent_sleep", timeout=20)
 
 web.get("https://lms.ssu.ac.kr/login")
-web.find(name="userid").send_keys(id)
-web.find(name="pwd").send_keys(pw, "\n")
+web.find(name="userid").send_keys(id, "\t", pw, "\n")
 web.find(text="마이페이지").up().click()
 
 web.goto_frame("/fulliframe")
 
 while True:
-    # Find the subject that has videos to watch
+    # 볼 영상이 있는 과목 찾기
     subject_btns = (
         web.find_all(tag="span", class_name="xntc-title", text="동영상")
         .up()
@@ -34,6 +35,7 @@ while True:
 
     selected = False
 
+    # 과목의 봐야할 동영상 강의 찾기
     for subject_btn in subject_btns:
         subject_btn.up(4).find(tag="button").click()
         titles = (
@@ -42,8 +44,8 @@ while True:
             .find(tag="a")
             .texts
         )
-        print(f"{titles=}")
         titles = [f"'{title}'" for title in titles if title not in ban_list]
+        print(*titles, sep=", ")
 
         if titles:
             selected = True
@@ -55,6 +57,7 @@ while True:
 
     subject_btn.click()
 
+    # 강의 보기
     for title in titles:
         web.goto_frame("/tool_content")
         web.find(tag="a", text=title).click()
@@ -70,8 +73,7 @@ while True:
             web.find(text="예").click()
             web.find(text="확인").click()
         print("Watching", title)
-        web.wait(dur=2)
-        # web.wait(dur=(cur_dur, total_dur))
+        web.wait((cur_dur, total_dur), display=True)
         web.back()
 
     web.goto_frame("/")
